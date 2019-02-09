54f462c8ff768419b284e59f18749e589602d983
John Phillips <jphillips37@gmail.com>
2019-02-09 10:35:25 -0500

    file structure created

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+drop database if exists burgers_db;
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers (
+    id integer not null auto_increment,
+    burger_name varchar(60),
+    devoured boolean default false,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ('bacon cheeseburger');
+insert into burgers (burger_name) values ('bonzai burger');
+insert into burgers (burger_name) values ('chili cheeseburger');
