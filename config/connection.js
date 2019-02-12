8d9d3283b3e0686bb98811ba065c56b8c71f2a1f
John Phillips <jphillips37@gmail.com>
2019-02-12 18:46:09 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "172.19.34.118",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "CWRU2019jp",
